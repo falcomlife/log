b24b223316c9a6bf62d7784b4a904f2241084d7a
sorawingwind <sorawingwind@163.com>
2021-02-18 10:04:35 +0800

    docs: dto dto

--- log/dto.go
+++ log/dto.go
@@ -33,27 +33,27 @@ type Node struct {
 }
 
 type Pod struct {
-	Name          string      `json:"name"`
-	Namespace     string      `json:"namespace"`
-	Node          string      `json:"node"`
-	CpuSumMax     float64     `json:"cpuSumMax"`
-	CpuSumMaxTime time.Time   `json:"cpuSumMaxTime"`
-	CpuSumMin     float64     `json:"cpuSumMin"`
-	CpuSumMinTime time.Time   `json:"cpuSumMinTime"`
-	CpuSumAvg     float64     `json:"cpuSumAvg"`
-	CpuVolatility float64     `json:"cpuVolatility"`
-	CpuMaxRatio   float64     `json:"cpuMaxRatio"`
-	CpuLaster     float64     `json:"cpuLaster"`
-	MemMax        float64     `json:"memMax"`
-	MemMaxTime    time.Time   `json:"memMaxTime"`
-	MemMin        float64     `json:"memMin"`
-	MemMinTime    time.Time   `json:"memMinTime"`
-	MemAvg        float64     `json:"memAvg"`
-	MemVolatility float64     `json:"memVolatility"`
-	MemMaxRatio   float64     `json:"memMaxRatio"`
-	MemLaster     float64     `json:"memLaster"`
-	Amplitude     float64     `json:"amplitude"`
-	Allocatable   Allocatable `json:"allocatable"`
+	Name          string      `json:"name"`          //名称
+	Namespace     string      `json:"namespace"`     //命名空间
+	Node          string      `json:"node"`          // 节点名
+	CpuSumMax     float64     `json:"cpuSumMax"`     // cpu各个核心总和最大值
+	CpuSumMaxTime time.Time   `json:"cpuSumMaxTime"` // cpu各个核心总和最大值发生时间
+	CpuSumMin     float64     `json:"cpuSumMin"`     // cpu各个核心总和最小值
+	CpuSumMinTime time.Time   `json:"cpuSumMinTime"` // cpu各个核心总和最小值发生时间
+	CpuSumAvg     float64     `json:"cpuSumAvg"`     // cpu各个核心平均值
+	CpuVolatility float64     `json:"cpuVolatility"` // cpu震动幅度
+	CpuMaxRatio   float64     `json:"cpuMaxRatio"`   // cpu值斜率最大
+	CpuLaster     float64     `json:"cpuLaster"`     // 上次cpu记录值
+	MemMax        float64     `json:"memMax"`        // 内存最大值
+	MemMaxTime    time.Time   `json:"memMaxTime"`    // 内存最大值发生时间
+	MemMin        float64     `json:"memMin"`        // 内存最小值
+	MemMinTime    time.Time   `json:"memMinTime"`    // 内存最小值发生时间
+	MemAvg        float64     `json:"memAvg"`        // 内存平均值
+	MemVolatility float64     `json:"memVolatility"` // 内存震动幅度
+	MemMaxRatio   float64     `json:"memMaxRatio"`   // 内存斜率最大值
+	MemLaster     float64     `json:"memLaster"`     // 上次内存记录值
+	Amplitude     float64     `json:"amplitude"`     // 波动
+	Allocatable   Allocatable `json:"allocatable"`   // node的资源可分配值
 }
 
 type WarningList struct {
